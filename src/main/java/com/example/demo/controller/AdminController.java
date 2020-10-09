83195d437f05adfa9b89827489e6fe581eba4a21
NgocLong18 <51955577+NgocLong18@users.noreply.github.com>
2020-10-09 11:00:19 +0700

    done phần người dùng chỉ còn đẩy lịch bên người dùng

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -9,7 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,11 +25,11 @@ public class AdminController {
     ISanBongRepository sanbnongservice;
 
     @GetMapping("/dsquanly")//hiển thị danh sách quản lý
-    public String listQuanly(Model model){
+    public String listQuanly(Model model) {
         List<NguoiDung> list = nguoidungservice.findAll();
         List<NguoiDung> listquanly = new ArrayList<NguoiDung>();
-        for(int i = 0; i < list.size();i++){
-            if ((list.get(i).getMaVaiTro()).equalsIgnoreCase("2")){
+        for (int i = 0; i < list.size(); i++) {
+            if ((list.get(i).getMaVaiTro()).equalsIgnoreCase("2")) {
                 listquanly.add(list.get(i));
             }
         }
@@ -34,6 +37,26 @@ public class AdminController {
         System.out.println("");
         return "admin/dsquanly";
     }
+
+    //Phần đăng ký cho quản lý mới
+    @GetMapping("/themquanly")//hiển thị form đăng ký cho quản lý mới
+    public ModelAndView savemanage(Model model) {
+        ModelAndView modelAndView = new ModelAndView("admin/themquanly");
+        modelAndView.addObject("nguoidung", new NguoiDung());
+        return modelAndView;
+    }
+
+    @PostMapping("/themquanly")//thực hiện action form đăng  ký quản lý mới, thay chỉ cho những thông tin cần thiết
+    public ModelAndView savemanage(@ModelAttribute("nguoidung") NguoiDung nguoiDung) {
+        nguoiDung.setMaVaiTro("2");
+        nguoiDung.setMatKhau("123");
+        nguoiDung.setTrangThai("active");
+        nguoidungservice.save(nguoiDung);
+        ModelAndView modelAndView = new ModelAndView("admin/themquanly");
+        modelAndView.addObject("message", "Lưu thành công");
+        return  modelAndView;
+    }
+
 
 
 
--- src/main/java/com/example/demo/controller/NguoidungConntroller.java
+++ src/main/java/com/example/demo/controller/NguoidungConntroller.java
@@ -19,23 +19,22 @@ public class NguoidungConntroller {
     private INguoiDungRepository nguoidungservice;
 
     @GetMapping("/login")//mấy cái này chắc ko cần nói
-    public ModelAndView saveStaffs(Model model){
+    public ModelAndView loginformanger(Model model) {
         ModelAndView modelAndView = new ModelAndView("login");
         modelAndView.addObject("nguoidung", new NguoiDung());
         return modelAndView;
     }
 
     @PostMapping("/login")//như trên
-    public ModelAndView login(@ModelAttribute("nguoidung")NguoiDung nguoiDung, @RequestParam("email") String email, @RequestParam("matKhau") String matKhau) {
+    public ModelAndView login(@ModelAttribute("nguoidung") NguoiDung nguoiDung, @RequestParam("email") String email, @RequestParam("matKhau") String matKhau) {
         List<NguoiDung> list = nguoidungservice.findAll();
         int maVaitro = 1;
         System.out.println(list.get(0).getEmail());
-        for (NguoiDung nguoiDung1 : list){
-            if(email.equalsIgnoreCase(nguoiDung1.getEmail()) && matKhau.equalsIgnoreCase(nguoiDung1.getMatKhau()) && (maVaitro+"").equalsIgnoreCase(nguoiDung1.getMaVaiTro()) ){
+        for (NguoiDung nguoiDung1 : list) {
+            if (email.equalsIgnoreCase(nguoiDung1.getEmail()) && matKhau.equalsIgnoreCase(nguoiDung1.getMatKhau()) && (maVaitro + "").equalsIgnoreCase(nguoiDung1.getMaVaiTro())) {
                 ModelAndView modelAndView = new ModelAndView("main");
-                maNguoidung= (int) nguoiDung1.getMaNguoiDung();
-                System.out.println(maNguoidung);
-                return modelAndView ;
+                maNguoidung = (int) nguoiDung1.getMaNguoiDung();
+                return modelAndView;
             }
         }
         ModelAndView modelAndView = new ModelAndView();
@@ -43,34 +42,57 @@ public class NguoidungConntroller {
         return modelAndView;
     }
 
-    @Autowired
-    INguoiDungRepository serviceNguoiDung;
+    //Phần hiển thị thông tin nguoi dung và upd thông tin người dùng
+    @GetMapping("/thongtinnguoidung")//chuyển form đổi mật khẩu đang test thử vs 1 cái nhất định
+    public ModelAndView hỉenthithongtin(){
+        if (maNguoidung == 0) {
+            ModelAndView modelAndView = new ModelAndView("login");
+            modelAndView.addObject("nguoidung", new NguoiDung());
+            return modelAndView;
+        }
+        Optional<NguoiDung> nguoiDung = nguoidungservice.findById(new Long(maNguoidung));
+        System.out.println(nguoiDung.get().getEmail());
+        ModelAndView modelAndView = new ModelAndView("nguoidung/information");
+        modelAndView.addObject("nguoidung", nguoiDung);
+        return modelAndView;
+    }
+
+    @PostMapping("/thongtinnguoidung")
+    public ModelAndView updthongtin(@ModelAttribute("nguoidung") NguoiDung nguoiDung, @RequestParam("tenNguoiDung") String tenNguoiDung, @RequestParam("soDienThoai") String soDienThoai, @RequestParam("diaChi") String diaChi) {
+        Optional<NguoiDung> thongtin = nguoidungservice.findById(new Long(maNguoidung));
+        String email = thongtin.get().getEmail();
+        String soDT1 = " ";
+        String maVaitro = thongtin.get().getMaVaiTro();
+        String trangThai = thongtin.get().getTrangThai();
+        String matkhau = thongtin.get().getMatKhau();
+        nguoiDung = new NguoiDung(new Long(maNguoidung), email, matkhau, tenNguoiDung, soDienThoai, soDT1, diaChi, maVaitro, trangThai);
+        nguoidungservice.save(nguoiDung);
+        ModelAndView modelAndView = new ModelAndView("nguoidung/information");
+        modelAndView.addObject("message", "Sửa thông tin thành công");
+        return modelAndView;
+    }
+
+    //Phần đổi mật khẩu người dùng
     @GetMapping("/doimatkhau")//chuyển form đổi mật khẩu đang test thử vs 1 cái nhất định
-    public ModelAndView editStaffs(){
-        System.out.println(maNguoidung);
-        Optional<NguoiDung> nguoiDung= serviceNguoiDung.findById(new Long(maNguoidung));
+    public ModelAndView editStaffs() {
+        if (maNguoidung == 0) {
+            ModelAndView modelAndView = new ModelAndView("login");
+            modelAndView.addObject("nguoidung", new NguoiDung());
+            return modelAndView;
+        }
+        Optional<NguoiDung> nguoiDung = nguoidungservice.findById(new Long(maNguoidung));
         ModelAndView modelAndView = new ModelAndView("nguoidung/changepass");
         modelAndView.addObject("nguoidung", new NguoiDung());
         return modelAndView;
     }
 
-    @PostMapping("/doimatkhau")//thực hiện đổi mà đang lỗi chết mẹ
-    public ModelAndView updateMatKhau(@ModelAttribute("nguoidung") NguoiDung nguoiDung,@RequestParam("matKhau") String matKhau) {
-        ModelAndView modelAndView = new ModelAndView("/doimatkhau");
-//        if (!matKhau.equalsIgnoreCase(nguoiDung.getMatKhau())) {
-//            modelAndView.addObject("message", "Sai mật khẩu");
-//            ModelAndView modelAndView1 = new ModelAndView("/doimatkhau");
-//        }
-////        else if(!XNMK.equalsIgnoreCase(MKM)){
-////            modelAndView.addObject("message1", "mât khẩu mới không khớp");
-////            ModelAndView modelAndView1 = new ModelAndView("nguoidung/changepass");
-////        }
-//        else {
-//            serviceNguoiDung.save(nguoiDung);
-//            ModelAndView modelAndView1 = new ModelAndView("/doimatkhau");
-//            modelAndView.addObject("message2", "Thay đổi thành công");
-//        }
-        serviceNguoiDung.save(nguoiDung);
+    @PostMapping("/doimatkhau")//thực hiện đổi mà đang lỗi chết mẹ -- hết lỗi rồi
+    public ModelAndView updateMatKhau(@ModelAttribute("nguoidung") NguoiDung nguoiDung, @RequestParam("matKhau") String matKhau) {
+        ModelAndView modelAndView = new ModelAndView("nguoidung/changepass");
+        Optional<NguoiDung> findperson = nguoidungservice.findById(new Long(maNguoidung));
+        nguoiDung = new NguoiDung(new Long(maNguoidung), findperson.get().getEmail(), matKhau, findperson.get().getTenNguoiDung(), findperson.get().getSoDienThoai(), findperson.get().getSoDienThoai1(), findperson.get().getDiaChi(), findperson.get().getMaVaiTro(), "active");
+        System.out.println(nguoiDung.getEmail());
+        nguoidungservice.save(nguoiDung);
         modelAndView.addObject("message", "Thay đổi thành công");
         return modelAndView;
     }
